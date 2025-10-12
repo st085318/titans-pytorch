3656a73635ece32ad430e9651d3ed7775a825de3
lucidrains <lucidrains@gmail.com>
2025-10-11 17:11:51 -0700

    Revert "fixes"
    This reverts commit 87771aaa2a967b9783eb2015652337e3752126cf.

--- titans_pytorch/neural_memory.py
+++ titans_pytorch/neural_memory.py
@@ -161,7 +161,8 @@ def newtonschulz5(
     eps = 1e-7,
     coefs = (3.4445, -4.7750, 2.0315)
 ):
-    t, inv_pack = pack_one_with_inverse(t, '* i j')
+    if t.ndim <= 3:
+        return t
 
     shape = t.shape
     should_transpose = shape[-2] > shape[-1]
@@ -169,6 +170,7 @@ def newtonschulz5(
     if should_transpose:
         t = t.transpose(-1, -2)
 
+    t, inv_pack = pack_one_with_inverse(t, '* i j')
     t = t / t.norm(dim = (-1, -2), keepdim = True).clamp(min = eps)
 
     a, b, c = coefs
